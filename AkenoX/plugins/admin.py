d7673a038d65ff886f0be0c78677fe719257b398
Drag <161552347+taslim19@users.noreply.github.com>
2025-03-04 12:00:16 +0530

    Update admin.py

--- AkenoX/plugins/admin.py
+++ AkenoX/plugins/admin.py
@@ -1,42 +1,32 @@
 from pyrogram.enums import ParseMode
+
 from AkenoX import *
 from AkenoX.plugins.helper.custom import get_user_info, temporary_mute_user
 from AkenoX.plugins.libso.funcs_admin import *
-from config import SUDO_USERS  # Import sudo users from config.py
-
-
-def is_sudo(user_id):
-    return user_id in SUDO_USERS
-
-
-def sudo_only(func):
-    async def wrapper(client, message):
-        if message.from_user.id not in SUDO_USERS:
-            return
-        return await func(client, message)
-    return wrapper
 
 
 @RENDYDEV.user(prefix=["id"], filters=(filters.me & ~filters.forwarded))
-@sudo_only
 async def id_handler(client, message):
     if message.reply_to_message and message.reply_to_message.from_user:
+        # Fetch User ID from the replied message
         user = message.reply_to_message.from_user
         user_info = f"<blockquote><b>User ID:</b> <code>{user.id}</code></blockquote>"
     else:
+        # Fetch Chat ID if no user is replied to
         chat = message.chat
         user_info = f"<blockquote><b>Chat ID:</b> <code>{chat.id}</code></blockquote>"
+
     await message.reply_text(user_info, parse_mode=ParseMode.HTML)
 
-
 @RENDYDEV.user(prefix=["info"], filters=(filters.me & ~filters.forwarded))
-@sudo_only
 async def userinfo_handler(client, message):
     if message.reply_to_message and message.reply_to_message.from_user:
         user = message.reply_to_message.from_user
     else:
         user = message.from_user
+
     chat = message.chat
+
     user_info = (
         f"Name: {user.first_name} {user.last_name or ''}\n"
         f"User ID: {user.id}\n"
@@ -45,55 +35,37 @@ async def userinfo_handler(client, message):
     )
     await message.reply_text(user_info)
 
-
 @RENDYDEV.user(prefix=["promote", "fullpromote"], filters=(filters.me & ~filters.forwarded))
-@sudo_only
 async def promoted_handler(client, message):
     await promotte_user(client, message)
 
-
 @RENDYDEV.user(prefix=["demote"], filters=(filters.me & ~filters.forwarded))
-@sudo_only
 async def demote_handler(client, message):
     await demote_user(client, message)
 
-
 @RENDYDEV.user(prefix=["ban", "dban"], filters=(filters.me & ~filters.forwarded))
-@sudo_only
 async def member_ban_handler(client, message):
     await member_ban_user(client, message)
 
-
 @RENDYDEV.user(prefix="unmute", filters=(filters.me & ~filters.forwarded))
-@sudo_only
 async def unmute_handler(client, message):
     await unmute_user(client, message)
 
-
 @RENDYDEV.user(prefix="pin", filters=(filters.me & ~filters.forwarded))
-@sudo_only
 async def pin_handler(client, message):
     await pin_message(client, message)
 
-
 @RENDYDEV.user(prefix=["mute", "dmute"], filters=(filters.me & ~filters.forwarded))
-@sudo_only
 async def mute_handler(client, message):
     await mute_user(client, message)
 
-
 @RENDYDEV.user(prefix=["kick", "dkick"], filters=(filters.me & ~filters.forwarded))
-@sudo_only
 async def kick_handler(client, message):
     await kick_user(client, message)
 
-
 @RENDYDEV.user(prefix=["tmute"], filters=(filters.me & ~filters.forwarded))
-@sudo_only
 async def tmute_handler(client, message):
-    await temporary_mute_user(client, message)  # Function to handle temporary mute
-
-
+    await temporary_mute_user(client, message)
 
 RENDYDEV.buttons(
     "admin",
